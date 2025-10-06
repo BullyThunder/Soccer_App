322f52be7b627b312f3d017242c14c0a4f9d3f8c
Bully_Thunder <vamosbarca24-7@gmail.com>
2025-10-06 09:18:19 +0300

    save

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const adminMatches = require('./src/routes/admin/matches')
 
 const app = express()
 app.use(cors({
-   origin: "http://localhost:3003", // адрес твоего локального фронта
+   origin: "http://localhost:3000", // адрес твоего локального фронта
   credentials: true                // важно для httpOnly cookie
 }));
 app.use(express.json())
