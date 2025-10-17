9466536570916e3b1647cc48fcb59b9ca2620418
Bully_Thunder <vamosbarca24-7@gmail.com>
2025-10-17 13:06:59 +0300

    save

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,11 +3,14 @@ const jwt = require('jsonwebtoken');
 const auth = async(req,res,next) =>{
     try{
       const token = req.cookies.token;
+      console.log('Cookies:', req.cookies);
         if (!token) return res.status(401).json({ error: 'No token provided' });
 
         const decoded = jwt.verify(token, process.env.SECRET)
         req.user = decoded;
         console.log(req.cookies) // ⚠ сохраняем информацию о пользователе
+        console.log("Cookies:", req.cookies);
+        console.log("Decoded user:", req.user);
         next();
     }
     catch(error){
